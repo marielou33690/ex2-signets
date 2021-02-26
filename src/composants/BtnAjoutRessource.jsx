247eefcaeb8e4641495ca34d3109b615613b1f89
Marie-Lou Denis <marielou33690@gmail.com>
2021-02-26 13:36:14 -0500

    ajout icon button et corrections css

--- src/composants/BtnAjoutRessource.jsx
+++ src/composants/BtnAjoutRessource.jsx
@@ -4,10 +4,10 @@ import AddIcon from '@material-ui/icons/Add';
 
 export default function BtnAjoutRessource() {
   return (
-    <button className="BtnAjoutRessource">
-      <Fab color="primary" size="medium" aria-label="add">
+    // <button className="BtnAjoutRessource">
+    <Fab color="primary" size="medium" aria-label="add" className="BtnAjoutRessource">
         <AddIcon />
     </Fab>
-    </button>
+    // </button>
   );
 }
--- src/composants/Dossier.jsx
+++ src/composants/Dossier.jsx
@@ -1,6 +1,7 @@
 import './Dossier.scss';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SortIcon from '@material-ui/icons/Sort';
+import IconButton from '@material-ui/core/IconButton';
 
 export default function Dossier({id, titre, couleur, dateModif}) {
   let styleCouleur = {
@@ -16,7 +17,9 @@ export default function Dossier({id, titre, couleur, dateModif}) {
         <h2>{titre}</h2>
         <p>Modifié : {dateModif}</p>
       </div>
-      <button><MoreVertIcon /></button>
+      <IconButton aria-label="plus">
+        <MoreVertIcon />
+      </IconButton>
     </article>
   );
 }
