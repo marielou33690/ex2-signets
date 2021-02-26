4cf0325b515c24201c6e480469b12f15193998e5
Marie-Lou Denis <marielou33690@gmail.com>
2021-02-26 14:00:13 -0500

    ajustements css suite a test sur gh pages

--- src/composants/BtnAjoutRessource.jsx
+++ src/composants/BtnAjoutRessource.jsx
@@ -4,10 +4,8 @@ import AddIcon from '@material-ui/icons/Add';
 
 export default function BtnAjoutRessource() {
   return (
-    // <button className="BtnAjoutRessource">
     <Fab color="primary" size="medium" aria-label="add" className="BtnAjoutRessource">
         <AddIcon />
     </Fab>
-    // </button>
   );
 }
